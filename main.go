847349837682fd238f5f5d311b1fbcc60e40ead9
Mahipal Panwar <mahipalpanwar6354@gmail.com>
2026-05-15 17:12:50 +0530

    main file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mahipalpanwar-7/TODO-RestAPI-DB/config"
+	"github.com/mahipalpanwar-7/TODO-RestAPI-DB/routes"
+	
+)
+
+func main() {
+	config.ConnectDB()
+	router := routes.Router()
+
+	fmt.Println("server running on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
